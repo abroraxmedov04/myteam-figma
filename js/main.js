cc95fab7528557695ae84d44fa9fa65ddd3f5ece
unknown <abrordevaxmedov@gmail.com>
2024-04-18 14:38:52 +0300

    header-completed

---
+++ js/main.js
@@ -0,0 +1,26 @@
+document.addEventListener("click", (e) => {
+  if (!e.target.matches(".header__menu-btn")) return;
+
+  let navigation = e.target.previousElementSibling;
+  let overlay = navigation.previousElementSibling;
+
+  navigation.classList.toggle("menu-show");
+  e.target.classList.toggle("change-img");
+
+  if (e.target.classList.contains("change-img")) {
+    e.target.style.backgroundImage = `url("../assets/images/close-menu-btn.png")`;
+    overlay.style.display = "block";
+  } else {
+    e.target.style.backgroundImage = `url("../assets/images/open-menu-btn.png")`;
+    overlay.style.display = "none";
+  }
+
+  overlay.addEventListener("click", () => {
+    navigation.classList.remove("menu-show");
+    e.target.classList.remove("change-img");
+    e.target.style.backgroundImage = `url("../assets/images/open-menu-btn.png")`;
+    overlay.style.display = "none";
+  });
+});
+
+
