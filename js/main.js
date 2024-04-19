f746eeb2affae3f894db1abbdfb6585194121740
unknown <abrordevaxmedov@gmail.com>
2024-04-19 23:02:22 +0300

    complete all section but not footer

--- js/main.js
+++ js/main.js
@@ -10,9 +10,11 @@ document.addEventListener("click", (e) => {
   if (e.target.classList.contains("change-img")) {
     e.target.style.backgroundImage = `url("../assets/images/close-menu-btn.png")`;
     overlay.style.display = "block";
+    document.body.style.overflow = "hidden";
   } else {
     e.target.style.backgroundImage = `url("../assets/images/open-menu-btn.png")`;
     overlay.style.display = "none";
+    document.body.style.overflow = "visible";
   }
 
   overlay.addEventListener("click", () => {
@@ -20,5 +22,24 @@ document.addEventListener("click", (e) => {
     e.target.classList.remove("change-img");
     e.target.style.backgroundImage = `url("../assets/images/open-menu-btn.png")`;
     overlay.style.display = "none";
+    document.body.style.overflow = "visible";
   });
+});
+
+document.addEventListener("click", (e) => {
+  if (!e.target.matches(".btn-rotate-card")) return;
+  let card = e.target.closest(".clients__card");
+
+  card.classList.toggle("active");
+
+  let backCard = card.querySelector(".clents__card-back");
+  if (card.classList.contains("active")) {
+    backCard.style.opacity = "100";
+    e.target.style.backgroundColor = "#f67e7e";
+    e.target.style.transform = "rotate(-45deg)";
+  } else {
+    backCard.style.opacity = "0";
+    e.target.style.backgroundColor = "#79c8c7";
+    e.target.style.transform = "rotate(0)";
+  }
 });
